94f72a046c4b0b2c0a679403c426f19c38237586
Matheus G. Dias <matheusgiga123@gmail.com>
2024-05-11 15:30:46 -0300

    feat: send email to new user

---
+++ src/main/java/com/ms/email/services/EmailService.java
@@ -0,0 +1,48 @@
+package com.ms.email.services;
+
+import com.ms.email.enums.StatusEmail;
+import com.ms.email.models.EmailModel;
+import com.ms.email.repositories.EmailRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service
+public class EmailService {
+    final EmailRepository emailRepository;
+    final JavaMailSender mailSender;
+
+    public EmailService(EmailRepository emailRepository, JavaMailSender mailSender) {
+        this.emailRepository = emailRepository;
+        this.mailSender = mailSender;
+    }
+
+    @Value(value = "${spring.mail.username}")
+    private String emailFrom;
+
+    @Transactional
+    public EmailModel sendEmail(EmailModel emailModel) {
+        try {
+            emailModel.setSendDateEmail(LocalDateTime.now());
+            emailModel.setEmailFrom(this.emailFrom);
+
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+
+            this.mailSender.send(message);
+
+            emailModel.setStatusEmail(StatusEmail.SENT);
+        } catch (MailException e) {
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+        }
+
+        return emailRepository.save(emailModel);
+    }
+}
